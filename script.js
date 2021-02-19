9bfac2a0ca3a6f28534f1ae8d56238958eee9c0f
Davide <davidelucifora1@gmail.com>
2021-02-19 18:14:38 +0530

    Add Player Factory

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-const gameBoard = (function(){
+//Player Factory
+const Player = (playerName, playerSign) => {
+    const getPlayerName = () => playerName
+    const getPlayerSign = () => playerSign
+    const getColor = () => {
+        if (getPlayerSign() === 'o') return 'orange'
+        else return 'blue'
+    }
+    return {getPlayerName, getPlayerSign, getColor}
+}
+const chooseNumberOfPlayers = (function(){
+    
 
+
+})();
+const gameBoard = (function(){
     const boardArray = [
         'x', 'o', 'x', 
         'x', 'o', 'o',
@@ -26,3 +40,14 @@ const gameBoard = (function(){
     }
     return {renderBoard}
 })();
+
+
+
+
+// Choose between one player or two player
+
+// OK FIRST WE NEED TO GET THE PLAYER TO CHOOSE HIS NAME AND SIGN. THIS WILL ASSIGN HIM A COLOR.
+// THEN THE BOARD WILL APPEAR, THE PLAYER WILL PUT HIS MARK SOMEWHERE WHICH MEANS:
+// WHEN HE CLICKS TO A CELL WITH A CERTAIN ID, WE GET THE NUMBER OF THAT CELL AND PLACE THAT SIGN IN THE ARRAY POSITION
+// WE THEN LOCK THE CELL AND MAKE IT UNCLICKABLE AGAIN. 
+// 
