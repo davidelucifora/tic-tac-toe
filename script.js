ddac6a5a57d38ad452a7caf3b340a50ce391132e
Davide <davidelucifora1@gmail.com>
2021-02-20 14:48:32 +0530

    Add PlayerSelection Module

--- script.js
+++ script.js
@@ -8,28 +8,51 @@ const Player = (playerName, playerSign) => {
     }
     return {getPlayerName, getPlayerSign, getColor}
 }
-const chooseNumberOfPlayers = (function(){
-    
 
+
+
+const chooseNumberOfPlayers = (function(){
+
+        let numberOfPlayers
+
+        const choiceModule = document.getElementById('choose-number-of-players');
+        const onePlayerButton = document.getElementById('choose-one-player')
+        const twoPlayersButton = document.getElementById('choose-two-players')
+
+    onePlayerButton.addEventListener('click', selectPlayer);
+    twoPlayersButton.addEventListener('click', selectPlayer)
+
+    function selectPlayer(e){
+        choiceModule.style.opacity = '0'
+        setTimeout(function(){choiceModule.remove()}, 400)
+        e.stopPropagation()
+        numberOfPlayers = this.dataset.selection
+        getNumberOfPlayers()
+    }
+
+    const getNumberOfPlayers = () => numberOfPlayers
+
+    return {getNumberOfPlayers}
 
 })();
+
 const gameBoard = (function(){
     const boardArray = [
         'x', 'o', 'x', 
         'x', 'o', 'o',
         'o', 'x', 'x'];
 
-    const cells = [...document.querySelectorAll('.cell')]
-    
-    function styleCell(cell) {
-        //Changes color based on cell content
-        if (cell.textContent === 'o') cell.classList.add('orange-text')
-        else cell.classList.add('blue-text');
-
+    const domElements =  {
+        cells: [...document.querySelectorAll('.cell')]
     }
+
     function renderBoard() {
+        populateBoard()
+    }
+
+    function populateBoard() {
         let index = 0
-        cells.forEach(cell => {
+        domElements.cells.forEach(cell => {
             //Assign unique incremental ID
             cell.id = 'cell-' + index;
             //Display sign from boardArray to cell.
@@ -38,11 +61,15 @@ const gameBoard = (function(){
             index++;
         })
     }
+
+    function styleCell(cell) {
+        //Changes color based on cell content
+        if (cell.textContent === 'o') cell.classList.add('orange-text')
+        else cell.classList.add('blue-text');
+
+    }
     return {renderBoard}
 })();
-
-
-
 
 // Choose between one player or two player
 
